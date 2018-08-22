af4411b0bd3ea14bc0f5bdc8ffbbc4a29a9c3da7
caikov <caikov@163.com>
2018-08-22 12:13:20 +0800

    change id to uuid
    Signed-off-by: caikov <caikov@163.com>

---
+++ oas-server/oas/src/main/java/com/cascv/oas/server/common/UuidPrefix.java
@@ -0,0 +1,8 @@
+package com.cascv.oas.server.common;
+
+public interface UuidPrefix {
+  public static final String USER_MODEL = "USR";
+  public static final String ENERGY_POINT = "EP";
+  public static final String ETH_WALLET = "EW";
+  public static final String USER_WALLET = "UW";
+}
