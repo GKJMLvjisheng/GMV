df4bd1382287858636abb5b6720d11515ced8fc5
caikov <caikov@163.com>
2018-08-31 14:35:09 +0800

    implement user wallet trascation detail
    Signed-off-by: caikov <caikov@163.com>

--- oas-server/oas/src/main/java/com/cascv/oas/server/blockchain/mapper/UserWalletDetailMapper.java
+++ oas-server/oas/src/main/java/com/cascv/oas/server/blockchain/mapper/UserWalletDetailMapper.java
@@ -7,7 +7,9 @@ import com.cascv.oas.server.blockchain.model.UserWalletDetail;
 public interface UserWalletDetailMapper {
   Integer insertSelective(UserWalletDetail userWalletDetail);
   List<UserWalletDetail> selectByUserUuid(String useruuid);
-  List<UserWalletDetail> selectByDateRange(@Param("userUuid") String userUuid, @Param("fromDate") String fromDate, @Param("toDate")String toDate);
+  
+  Integer selectCount();
+  List<UserWalletDetail> selectByPage(@Param("userUuid") String userUuid, @Param("offset") Integer offset, @Param("limit") Integer limit);
   Integer deleteByUserUuid(String userUuid);
   Integer deleteByUuid(String uuid);
 }
