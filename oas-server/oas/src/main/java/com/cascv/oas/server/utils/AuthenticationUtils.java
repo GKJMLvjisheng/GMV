19aa9181f581f2f56feacd8143e392ae4c335ea7
lvjisheng <lvjishenghnb@163.com>
2018-09-06 11:08:40 +0800

    11:08

--- oas-server/oas/src/main/java/com/cascv/oas/server/utils/AuthenticationUtils.java
+++ oas-server/oas/src/main/java/com/cascv/oas/server/utils/AuthenticationUtils.java
@@ -2,12 +2,12 @@ package com.cascv.oas.server.utils;
 
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
-import com.cascv.oas.server.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
-import com.cascv.oas.server.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
 
 /*
  * Name:AuthenticationUtils
