fe6c25c5b8dc52c9f74a22ee4828e7c8a720df25
qqq <douniu@163.com>
2018-09-17 17:04:46 +0800

    add EnergyPowerControlle

---
+++ oas-server/oas/src/main/java/com/cascv/oas/server/energy/controller/EnergyPowerController.java
@@ -0,0 +1,44 @@
+package com.cascv.oas.server.energy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cascv.oas.core.common.ErrorCode;
+import com.cascv.oas.core.common.ResponseEntity;
+import com.cascv.oas.server.energy.model.EnergyWallet;
+import com.cascv.oas.server.energy.service.EnergyService;
+import com.cascv.oas.server.utils.ShiroUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping(value = "/api/v1/energyPower")
+@Slf4j
+public class EnergyPowerController {
+	
+	@Autowired
+    private EnergyService energyService;
+	
+	@PostMapping(value = "/inquirePower")
+    @ResponseBody
+    public ResponseEntity<?> inquirePower() {
+        EnergyWallet energyWallet = energyService.findByUserUuid(ShiroUtils.getUserUuid());
+        if (energyWallet != null) {
+            return new ResponseEntity.Builder<Integer>()
+                    .setData(energyWallet.getPower().intValue())
+                    .setErrorCode(ErrorCode.SUCCESS)
+                    .build();
+        } else {
+            return new ResponseEntity.Builder<Integer>()
+                    .setData(0)
+                    .setErrorCode(ErrorCode.NO_ENERGY_POINT_ACCOUNT)
+                    .build();
+        }
+    }
+	
+	
+
+}
