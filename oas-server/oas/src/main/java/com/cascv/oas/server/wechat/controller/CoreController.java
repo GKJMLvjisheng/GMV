019d0b88a8427f4b4ea2da6aab01b3cacaa663d0
qqq <douniu@163.com>
2018-09-19 09:30:21 +0800

    up

--- oas-server/oas/src/main/java/com/cascv/oas/server/wechat/controller/CoreController.java
+++ oas-server/oas/src/main/java/com/cascv/oas/server/wechat/controller/CoreController.java
@@ -1,42 +1,24 @@
 package com.cascv.oas.server.wechat.controller;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.ibatis.annotations.Param;
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cascv.oas.server.energy.controller.ComputingPowerController;
 import com.cascv.oas.server.utils.ShiroUtils;
 import com.cascv.oas.server.wechat.Service.WechatService;
 import com.cascv.oas.server.wechat.Utils.StringUtils;
 import com.cascv.oas.server.wechat.vo.IdenCodeDomain;
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 
 import lombok.extern.slf4j.Slf4j;
-import net.sf.json.JSONObject;
 @Slf4j
 @RestController
 @RequestMapping(value="/api/v1/wx")
