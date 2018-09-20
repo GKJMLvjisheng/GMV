3af719ae68748cd63185f7f64204b88f48c3a755
yangming9 <yangming21_ok@163.com>
2018-09-20 10:08:36 +0800

    fix news

--- oas-h5/src/views/Index.vue
+++ oas-h5/src/views/Index.vue
@@ -125,6 +125,7 @@ const attendanceSuccess = require("@/assets/images/attendance.png");
 const energyBall = require("@/assets/images/ball.png");
 
 import { randomNum } from '@/utils/utils.js'
+import Math from 'mathjs'
 export default {
   name: "index",
   data() {
@@ -145,6 +146,7 @@ export default {
       currentEnergy:0,
       currentPower:0,
       page:1,
+      newsTotal:1,
       articleList:[],
       analysis:'',
       analysisCount:0,
@@ -186,7 +188,8 @@ export default {
       formData.append("pageSize", "3");
       this.$axios.post('/energyPoint/inquireNews',formData)
       .then(({data:{data}}) =>{
-        //console.log(data.msg);
+        console.log(data);
+        this.newsTotal=data.data.total;
         if(data.msg=="无更多数据"){
           this.isShowNewsTip=false;
           this.isShowNoNews=true;    
@@ -202,16 +205,20 @@ export default {
       })
     },   
 
-     //上拉加载新闻
+   //上拉加载新闻
    infinite (done) { 
       this.page+=1
       var page=this.page
-      //alert(page);
-      this.isShowNewsTip=true                     
-      this.loadArticleList(page)  
-      setTimeout(() => {
-        done()
-      },1000)
+      var newsTotal=this.newsTotal     
+      var pageTotal=Math.ceil(newsTotal/3)
+      //alert(pageTotal)
+      if(page<=pageTotal){
+        this.isShowNewsTip=true                     
+        this.loadArticleList(page)  
+        setTimeout(() => {
+          done()
+        },1000)
+      }      
     },
 
     // 签到按钮点击事件
@@ -529,14 +536,14 @@ header {
     a {
         display: flex;
         justify-content: space-between;
-        height: 352px;
+        height: 280px;
         padding: 40px 0;
         border-bottom: 1px solid #ddd;    
         word-wrap:break-word;
         word-break:break-all;
       img {
-        width: 448px;
-        height: 256px;
+        width: 248px;
+        height: 156px;
         margin-left: 26px;
       }
       .left {
@@ -563,7 +570,7 @@ header {
             flex: 1;
             font-size: 24px;
             line-height: 34px;
-            margin-top: 40px;
+            margin-top: 30px;
             overflow: hidden;
             text-overflow: ellipsis;
             display: -webkit-box;
