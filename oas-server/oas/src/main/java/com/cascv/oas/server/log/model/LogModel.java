806dd1991e9103445ca798f7b22a23e3d25e93a1
tocai <tongyin.cai@nokia-sbell.com>
2018-09-23 10:42:06 +0800

    support log to db
    Signed-off-by: tocai <tongyin.cai@nokia-sbell.com>

--- oas-server/oas/src/main/java/com/cascv/oas/server/log/model/LogModel.java
+++ oas-server/oas/src/main/java/com/cascv/oas/server/log/model/LogModel.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import lombok.Getter;
 import lombok.Setter;
 
-public class LogEntry implements Serializable {
+public class LogModel implements Serializable {
   private static final long serialVersionUID = 1L;
+  @Getter @Setter private String uuid;
   @Getter @Setter private String user;
-  @Getter @Setter private String time;
+  @Getter @Setter private String host;
+  @Getter @Setter private String sessionId;
   @Getter @Setter private String action;
   @Getter @Setter private String comment;
+  @Getter @Setter private String time;
 }
