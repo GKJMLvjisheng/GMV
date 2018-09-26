13373997e3f0f214483eac59478ba0f35be41c83
qqq <douniu@163.com>
2018-09-26 14:00:11 +0800

    add getReward

---
+++ oas-server/oas/src/main/java/com/cascv/oas/server/activity/wrapper/ActivityGetReward.java
@@ -0,0 +1,14 @@
+package com.cascv.oas.server.activity.wrapper;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class ActivityGetReward implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Getter @Setter private Integer sourceCode;
+
+}
