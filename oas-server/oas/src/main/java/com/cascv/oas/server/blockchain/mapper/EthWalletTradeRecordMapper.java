0ca0cf70f279f89f44e97173f3a48a2bebcde779
ym <yangming21_ok@163.com>
2018-09-27 17:00:39 +0800

    add inqureEthWalletInTotalTradeRecord and out

--- oas-server/oas/src/main/java/com/cascv/oas/server/blockchain/mapper/EthWalletTradeRecordMapper.java
+++ oas-server/oas/src/main/java/com/cascv/oas/server/blockchain/mapper/EthWalletTradeRecordMapper.java
@@ -1,10 +1,13 @@
 package com.cascv.oas.server.blockchain.mapper;
 import java.util.List;
 
+import com.cascv.oas.server.blockchain.wrapper.EthWalletTotalTradeRecordInfo;
 import com.cascv.oas.server.blockchain.wrapper.EthWalletTradeRecordInfo;;
 
 public interface EthWalletTradeRecordMapper {
 	
 	List<EthWalletTradeRecordInfo> selectAllTradeRecord();
+	List<EthWalletTotalTradeRecordInfo> selectAllInTotalTradeRecord();
+	List<EthWalletTotalTradeRecordInfo> selectAllOutTotalTradeRecord();
 	
 }
