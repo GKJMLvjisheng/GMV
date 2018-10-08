37c567a9ce963c1d83e8af62b4b136cd6c810cb6
zhl123 <923471788@qq.com>
2018-10-08 17:41:45 +0800

    up

--- oas-h5/src/main.js
+++ oas-h5/src/main.js
@@ -8,14 +8,14 @@ import Axios from 'axios'
 import VueScroller from 'vue-scroller'
 //import router from 'vue-router' 
 Vue.use(VueScroller)
-/*var token = null
+var token = null
 if(window.Android) {
   token = window.Android.getToken()
-}*/
+}
 Axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
 Axios.interceptors.request.use(config => {
   config.headers['Content-type'] = 'application/json;charset=UTF-8'
-  //config.headers['token'] = token || '99e04e02-fb5c-4f65-a6cf-a93006c351b6'
+  config.headers['token'] = token || '99e04e02-fb5c-4f65-a6cf-a93006c351b6'
    /*config.headers = {
     
      'Content-Type': 'application/json;charset=utf-8',
