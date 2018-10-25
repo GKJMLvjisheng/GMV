1522d449298177201b6dfc42193c14c04049b124
liruimei <15700190122@163.com>
2018-10-25 13:57:05 +0800

    update userAccount

--- oas-server/oas/src/main/resources/static/js/userAccount/userAccountTable.js
+++ oas-server/oas/src/main/resources/static/js/userAccount/userAccountTable.js
@@ -360,21 +360,21 @@ function initSystemGrid() {
 				field : "mobile",
 				align: 'center',
 				valign: 'middle',
-				width:  '100px',
+				width:  '120px',
 			},
 			{
 				title : "邮箱",
 				field : "email",
 				align: 'center',
 				valign: 'middle',
-				width:  '93px',
+				width:  '120px',
 			},
 			{
 				title : "IMEI",
 				field : "imei",
 				align: 'center',
 				valign: 'middle',
-				width:  '93px',
+				width:  '120px',
 				
 			},
 				{
@@ -392,23 +392,25 @@ function initSystemGrid() {
 				valign: 'middle',
 				width:  '50px',
 				formatter: actionFormatter2
-			},{
-
-				title : "角色授权",
-				field : "name",
-				align: 'center',
-				valign: 'middle',
-				width:  '80px',
-				formatter: actionFormatter3
-			},{
-
-				title : "账号状态",
-				field : "name",
-				align: 'center',
-				valign: 'middle',
-				width:  '80px',
-				formatter: actionFormatter4
-			}],		
+			}
+//			,{
+//
+//				title : "角色授权",
+//				field : "name",
+//				align: 'center',
+//				valign: 'middle',
+//				width:  '80px',
+//				formatter: actionFormatter3
+//			},{
+//
+//				title : "账号状态",
+//				field : "name",
+//				align: 'center',
+//				valign: 'middle',
+//				width:  '80px',
+//				formatter: actionFormatter4
+//			}
+			],		
 //		  search : true,//搜索
 //        searchOnEnterKey : true,
 		clickToSelect: false,         
