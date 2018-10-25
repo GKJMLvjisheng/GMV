d750fb8c18df0de535984a40dbfbd28c525d25b1
zhl123 <923471788@qq.com>
2018-10-25 15:24:50 +0800

    up

--- oas-h5/vue.config.js
+++ oas-h5/vue.config.js
@@ -5,7 +5,8 @@ module.exports = {
             '/api': {
                 //target: 'https://oas.cascv.com/api/v1',
                 //target: 'http://localhost:8080/api/v1',
-                target: 'https://localhost/api/v1',
+                target: 'https://dapp.oases.pro/api/v1',
+                //target: 'https://localhost/api/v1',
                 changeOrigin: true,
                 ws: true,
                 pathRewrite: {
