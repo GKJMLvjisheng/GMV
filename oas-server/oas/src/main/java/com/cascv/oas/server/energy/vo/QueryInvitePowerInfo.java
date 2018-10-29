c4716f77e3c3e98e64ee6ddf82b055a0c73c0b06
ym <yangming21_ok@163.com>
2018-10-29 18:00:47 +0800

    fix

--- oas-server/oas/src/main/java/com/cascv/oas/server/energy/vo/QueryInvitePowerInfo.java
+++ oas-server/oas/src/main/java/com/cascv/oas/server/energy/vo/QueryInvitePowerInfo.java
@@ -1,6 +1,7 @@
 package com.cascv.oas.server.energy.vo;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +10,6 @@ public class QueryInvitePowerInfo implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Getter @Setter private Integer SumUserInvited;//邀请总人数
-	@Getter @Setter private Integer SumPowerPromoted;//累计算力提升
+	@Getter @Setter private BigDecimal SumPowerPromoted;//累计算力提升
 	
 }
